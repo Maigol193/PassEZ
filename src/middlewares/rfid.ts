39e2231572b36dd37c2ac8030a2c151386277dc3
Andres <Andresblancocontact@gmail.com>
2024-10-25 13:14:11 -0600

    RFDI ahora recibe y regresa el valor de UID de la tarjeta

--- src/middlewares/rfid.ts
+++ src/middlewares/rfid.ts
@@ -3,18 +3,19 @@ import { HTTP_STATUS_CODES } from '../types/http-status-codes';
 
 class RFIDMiddleware {
     generateRFIDTag(req: Request, res: Response, next: NextFunction) {
-        const { userId } = req.body; // userId es el key o campo que recibe el body en la consulta a la ruta
+        const { userId, uid } = req.body; // userId y uid son los campos que recibe el body en la consulta a la ruta
 
-        if (!userId) {
-            res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({ message: 'userId es requerido' });
+        if (!userId || !uid) {
+            res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({ message: 'userId y uid son requeridos' });
             return;
         }
 
         // Generar un tag RFID único
         const rfidTag = Math.random().toString(36).substring(2, 12);
 
-        // Agregar el tag RFID generado al cuerpo de la solicitud
+        // Agregar el tag RFID generado y el uid al cuerpo de la solicitud
         req.body.rfidTag = rfidTag;
+        req.body.uid = uid; // uid es el valor de la tarjeta para asociar la entrada con una tarjeta
         next();  // Continuar hacia el controlador
     }
 }
