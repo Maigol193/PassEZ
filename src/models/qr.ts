0b5374eb7ba11c32da5da553ce7c92886326d8e5
Maigol <migueltorrespvr2@gmail.com>
2024-10-29 21:09:00 -0600

    QR & Numpad Codes Done!

--- src/models/qr.ts
+++ src/models/qr.ts
@@ -3,7 +3,8 @@ const { Schema, model, SchemaTypes } = require('mongoose');
 const qrSchema = new Schema({
     userId: { type: SchemaTypes.String, required: true },
     qrCode: { type: SchemaTypes.String, required: true },
-    createdAt: { type: SchemaTypes.Date, default: Date.now, expires: '1h' } // El código expira en 1 hora
+    createdAt: { type: SchemaTypes.Date, default: Date.now, expires: '24h' } // La etiqueta RFID expira en 1 hora
+
 });
 const qr = model('qr', qrSchema);
 export default qr;
