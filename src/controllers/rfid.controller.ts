092be73b927e28fb02383bd07a23a25f9a3fe7cd
Maigol <migueltorrespvr2@gmail.com>
2024-11-06 12:48:12 -0600

    pruebas rfid 5

--- src/controllers/rfid.controller.ts
+++ src/controllers/rfid.controller.ts
@@ -14,7 +14,7 @@ class rfidController {
         // }
 
         // Generar un tag RFID único
-        const rfidTag = Math.random().toString(36).substring(2, 12);
+        const rfidTag = Math.random().toString(36).substring(2, 13);
 
         // Crear una nueva entrada en la base de datos
         const newRFID = new rfid({
